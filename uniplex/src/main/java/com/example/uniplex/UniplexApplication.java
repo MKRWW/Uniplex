de6da25dcae1c812461bbd974e72ebfe71da87ce
Markus Kremer <markus.kremer@cortex-ag.com>
2024-02-28 10:56:42 +0100

    [OP-53] Neues-Basis-Service-anlegen

---
+++ uniplex/src/main/java/com/example/uniplex/UniplexApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniplexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniplexApplication.class, args);
+    }
+
+}
---
+++ uniplex/src/test/java/com/example/uniplex/UniplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniplexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
